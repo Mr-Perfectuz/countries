de28031fe5a9d211cd95977d30af4434b2a56224
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-12-05 02:53:22 +0900

    add pictures, icons and adjust sizes of main

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+const elDarkmodeButton = document.querySelector('dark-mode-button');
+
+elDarkmodeButton.addEventListener('click', function(){
+    // document.body.classList.add('dark-mode');
+});
+
+// elDarkmodeButton.addEventListener('click', function(){
+//     document.body.classList.remove('dark-mode');
+// });
